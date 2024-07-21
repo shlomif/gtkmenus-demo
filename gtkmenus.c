da4d172dad4590601e550e47d7d3d4398ffbc717
Shlomi Fish <shlomif@shlomifish.org>
2024-07-21 13:38:25 +0300

    remove more unused code; prog still running

--- gtkmenus.c
+++ gtkmenus.c
@@ -495,33 +495,6 @@ out:
   return 0;
 }
 
-static void
-print_version (void)
-{
-  g_print ("gtk3-demo %d.%d.%d\n",
-           gtk_get_major_version (),
-           gtk_get_minor_version (),
-           gtk_get_micro_version ());
-}
-
-static int
-local_options (GApplication *app,
-               GVariantDict *options,
-               gpointer      data)
-{
-  gboolean version = FALSE;
-
-  g_variant_dict_lookup (options, "version", "b", &version);
-
-  if (version)
-    {
-      print_version ();
-      return 0;
-    }
-
-  return -1;
-}
-
 int
 main (int argc, char **argv)
 {
@@ -558,7 +531,6 @@ main (int argc, char **argv)
   g_signal_connect (app, "startup", G_CALLBACK (startup), NULL);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   g_signal_connect (app, "command-line", G_CALLBACK (command_line), NULL);
-  g_signal_connect (app, "handle-local-options", G_CALLBACK (local_options), NULL);
 
   g_application_run (G_APPLICATION (app), argc, argv);
 
